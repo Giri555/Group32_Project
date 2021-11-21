3f18001c90c987f1b46905347cde45f40a144faf
Giri Siva <giri_siva51@outlook.com>
2021-11-20 21:44:38 -0500

    Cleaned

--- Group32_Client/Controllers/HomeController.cs
+++ Group32_Client/Controllers/HomeController.cs
@@ -23,11 +23,6 @@ namespace Group32_Client.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
