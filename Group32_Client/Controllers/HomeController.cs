8e885d5f4bb730701f380f535f01e6d4ec88ce62
Giri Siva <giri_siva51@outlook.com>
2021-11-21 00:17:42 -0500

    Instantiate httpclient factory + consuming named client example

--- Group32_Client/Controllers/HomeController.cs
+++ Group32_Client/Controllers/HomeController.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace Group32_Client.Controllers
@@ -12,14 +13,34 @@ namespace Group32_Client.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+        private const string NAMED_CLIENT = "project_api";
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory clientFactory)
         {
             _logger = logger;
+            _clientFactory = clientFactory;
         }
 
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Example() // Example consuming a named client
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/destination");
+            var client = _clientFactory.CreateClient(NAMED_CLIENT);
+            var response = await client.SendAsync(request);
+            if(response.IsSuccessStatusCode)
+            {
+
+            }
+            else
+            {
+
+            }
             return View();
         }
 
